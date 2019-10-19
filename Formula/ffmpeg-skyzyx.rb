ae5cd1b9f27fd9e11f10c6c351cd361695aea737
Ryan Parman <ryan@ryanparman.com>
2019-10-19 14:25:32 -0700

    Testing

--- Formula/ffmpeg-skyzyx.rb
+++ Formula/ffmpeg-skyzyx.rb
@@ -266,7 +266,6 @@ class FfmpegSkyzyx < Formula
 
     # system "LIBFFI_CFLAGS=\"#{ENV["LIBFFI_CFLAGS"]}\"", "LIBFFI_LIBS=\"#{ENV["LIBFFI_LIBS"]}\"", "GLIB_CFLAGS=\"#{ENV["GLIB_CFLAGS"]}\"", "GLIB_LIBS=\"#{ENV["GLIB_LIBS"]}\"", "./configure", *args
     system "./configure", *args
-    system "make"
     system "make", "install"
 
     # Build and install additional FFmpeg tools
